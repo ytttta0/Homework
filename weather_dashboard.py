e74562f481f59f926fdbb19feaf4bc614cbed991
ytttta0 <ytung1004@gmail.com>
2025-11-15 16:14:46 +0800

    Update weather_dashboard.py

--- weather_dashboard.py
+++ weather_dashboard.py
@@ -7,7 +7,7 @@ st.title("⛅ 台灣氣象資料 Dashboard")
 API_KEY = "CWA-7539DAF4-828A-4271-9F98-CCE621362C83"
 LOCATION = st.selectbox("選擇城市",["臺北市","臺中市","高雄市"])
 
-url = f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={API_KEY}&locationName=臺北市"
+url = f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={API_KEY}&locationName={LOCATION}"
 res = requests.get(url, verify=False)
 data = res.json()
 
@@ -18,6 +18,7 @@ for element in location["weatherElement"]:
     name = element["elementName"]
     value = element["time"][0]["parameter"]["parameterName"]
     st.write(f"{name}:{value}")
+
 
 
 
